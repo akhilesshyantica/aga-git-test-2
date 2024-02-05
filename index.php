754712fde856e36ce47a64789b5ba43d2ab2c7a6
Akhiles Shyantica Wisnumurti <akhilesshyanticaw@gmail.com>
2024-02-05 13:42:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem 
             provident officiis ut maiores quos? Exercitationem 
             labore necessitatibus quasi aut error.</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+        </ul>
 </body>
 </html>
